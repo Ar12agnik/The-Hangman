cbccaaee833ee6b8c30a70067e00b5afea1efa53
Ar12agnik <102238499+Ar12agnik@users.noreply.github.com>
2023-03-20 12:15:07 +0530

    Update Non_gui_part.py

--- main/Non_gui_part.py
+++ main/Non_gui_part.py
@@ -1,1 +1,12 @@
 #ayanabha
+def slelct_random_word():
+    pass#returns the random word
+def check_score():
+    pass#returns score (how many times won,lost)
+def check_letter():
+    pass#returns true when letter in word else returns false
+def check_rem_attempts():
+    pass#returns remaining no of attempts
+def main():
+    pass#main game(loop)
+
