e01e6b81f2cdea1679cbbea9d8ee0c8624bc7804
Ar12agnik <102238499+Ar12agnik@users.noreply.github.com>
2023-03-21 10:28:16 +0530

    update

--- main/gui_part.py
+++ main/gui_part.py
@@ -1,1 +1,17 @@
 #agnik part
+import pygame as pg
+import sys
+pg.init()
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+screen=pg.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+screen.fill((255, 255, 255))
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+            
+        
+
+        pg.display.update()
---
+++ main/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+            screen_width = screen_info.current_w
+            screen_height = screen_info.current_h
+
+            # create the fullscreen display surface
+            screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
