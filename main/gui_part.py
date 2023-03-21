f5a4bc9f1be83680bef4339502244a75d2168c51
Ar12agnik <102238499+Ar12agnik@users.noreply.github.com>
2023-03-21 11:04:40 +0530

    Update gui_part.py

--- main/gui_part.py
+++ main/gui_part.py
@@ -2,16 +2,23 @@
 import pygame as pg
 import sys
 pg.init()
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
-screen=pg.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-screen.fill((255, 255, 255))
-while True:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
-            
-        
+WINDOW_WIDTH = 788
+WINDOW_HEIGHT = 493
 
-        pg.display.update()
+z=int(input("enter a number: "))
+def gui_screen(z):
+    screen=pg.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+    screen.fill((255, 255, 255))
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            x=pg.image.load(f'main\\{z}.png').convert_alpha()
+            screen.blit(x,(0,0))
+                
+            
+
+            pg.display.update()
+if __name__=='__main__':
+    gui_screen(z)
